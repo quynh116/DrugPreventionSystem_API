2f4cd11cda4e156ee1200e326fa42412f95721a4
Welcome <quynhphama99@gmail.com>
2025-06-12 17:36:05 +0700

    test

--- DrugPreventionSystem.API/Program.cs
+++ DrugPreventionSystem.API/Program.cs
@@ -56,7 +56,7 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 // Token
-var serect = builder.Configuration["AppSettings:SecretKey"];
+var serect = builder.Configuration["AppSettings:SecretKe"];
 var key = Encoding.ASCII.GetBytes(serect);
 builder.Services.AddAuthentication(x =>
 {
