b735274e65d50d19712d69cd3127871829783764
Bao-2005 <doquocbao2009@gmail.com>
2025-06-13 01:30:33 +0700

    Survey

---
+++ DrugPreventionSystem.BusinessLogic/Models/Request/Survey/SurveyUpdateRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugPreventionSystem.BusinessLogic.Models.Request.Survey
+{
+    public class SurveyUpdateRequest
+    {
+        public Guid SurveyId { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ DrugPreventionSystem.BusinessLogic/Services/Interfaces/ISurveyService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrugPreventionSystem.BusinessLogic.Commons;
+using DrugPreventionSystem.BusinessLogic.Models.Request.Survey;
+using DrugPreventionSystem.BusinessLogic.Models.Responses.Survey;
+using DrugPreventionSystem.DataAccess.Models;
+
+namespace DrugPreventionSystem.BusinessLogic.Services.Interfaces
+{
+    public interface ISurveyService
+    {
+        Task<Result<IEnumerable<SurveyResponse>>> GetAllSurveyAsync();
+        Task<Result<SurveyResponse>> GetSurveyByIdAsync(Guid id);
+        Task<Result<bool>> DeleteSurveyByIdAsync(Guid id);
+        Task<Result<SurveyResponse>> UpdateSurveyAsync(Guid id, SurveyUpdateRequest request);
+    }
+}
