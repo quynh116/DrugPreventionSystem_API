8ab9981f26fa278556d8b93bdc7c2b1310bfcfd6
ThinhNguyen <thinhnguyen123258@gmail.com>
2025-06-13 17:51:25 +0700

    add CreateSurveyQuestion

--- DrugPreventionSystem.API/Controllers/SurveyQuestionController.cs
+++ DrugPreventionSystem.API/Controllers/SurveyQuestionController.cs
@@ -5,6 +5,9 @@ using DrugPreventionSystem.BusinessLogic.Models.Responses;
 using DrugPreventionSystem.BusinessLogic.Models.Responses.Consultant;
 using DrugPreventionSystem.BusinessLogic.Services;
 using DrugPreventionSystem.BusinessLogic.Services.Interfaces;
+using DrugPreventionSystem.DataAccess.Models;
+using DrugPreventionSystem.DataAccess.Repository;
+using DrugPreventionSystem.DataAccess.Repository.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,11 +18,29 @@ namespace DrugPreventionSystem.API.Controllers
     public class SurveyQuestionController : BaseApiController
     {
         private readonly ISurveyQuestionService _surveyQuestionService;
-        public SurveyQuestionController(ISurveyQuestionService surveyQuestionService)
+        private readonly ISurveyQuestionRepository _surveyQuestionRepository;
+
+        public SurveyQuestionController(ISurveyQuestionService surveyQuestionService, ISurveyQuestionRepository surveyQuestionRepository)
         {
             _surveyQuestionService = surveyQuestionService;
+            _surveyQuestionRepository = surveyQuestionRepository;
         }
-        
+
+        private SurveyQuestionResponse MapToResponse(SurveyQuestion question)
+        {
+            return new SurveyQuestionResponse
+            {
+                QuestionId = question.QuestionId,
+                SurveyId = question.SurveyId,
+                QuestionText = question.QuestionText,
+                QuestionType = question.QuestionType,
+                Sequence = question.Sequence,
+                CreatedAt = question.CreatedAt,
+                UpdatedAt = question.UpdatedAt
+            };
+        }
+
+
 
         [HttpGet]
         public async Task<ActionResult<Result<IEnumerable<SurveyQuestionResponse>>>> GetAllSurveyQuestions()
@@ -35,7 +56,34 @@ namespace DrugPreventionSystem.API.Controllers
             return HandleResult(result);
         }
 
-        [HttpPut("{id}")]
+        [HttpPost]
+        public async Task<Result<SurveyQuestionResponse>> AddSurveyQuestionAsync(SurveyQuestionCreateRequest request)
+        {
+          try
+          {
+            var newQuestion = new SurveyQuestion
+            {
+                QuestionId = Guid.NewGuid(),
+                SurveyId = request.SurveyId,
+                QuestionText = request.QuestionText,
+                QuestionType = request.QuestionType,
+                Sequence = request.Sequence,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            await _surveyQuestionRepository.AddSurveyQuestion(newQuestion);
+
+            return Result<SurveyQuestionResponse>.Success(MapToResponse(newQuestion), "Survey question added.");
+          }
+          catch (Exception ex)
+          {
+            return Result<SurveyQuestionResponse>.Error($"Error adding survey question: {ex.Message}");
+        }
+    }
+
+
+    [HttpPut("{id}")]
         public async Task<ActionResult<Result<SurveyQuestionResponse>>> UpdateSurveyQuestion(Guid id, [FromBody] SurveyQuestionUpdateRequest request)
         {
             if (!ModelState.IsValid)
@@ -49,7 +97,7 @@ namespace DrugPreventionSystem.API.Controllers
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Result<bool>>> DeleteConsultant(Guid id)
+        public async Task<ActionResult<Result<bool>>> DeleteSurveyQuestion(Guid id)
         {
             var result = await _surveyQuestionService.DeleteSurveyQuestionAsync(id);
             if (result.ResultStatus == ResultStatus.Success)
