d01aa8476c9921896cce42105982f11f71cd9637
Bao-2005 <doquocbao2009@gmail.com>
2025-06-29 20:52:50 +0700

    Added Community Program Repository

---
+++ DrugPreventionSystem.DataAccess/Repository/Interfaces/ICommunityProgramRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrugPreventionSystem.DataAccess.Models;
+
+namespace DrugPreventionSystem.DataAccess.Repository.Interfaces
+{
+    public interface ICommunityProgramRepository
+    {
+        Task<CommunityProgram> GetProgramById(Guid id);
+        Task<IEnumerable<CommunityProgram>> GetAllPrograms();
+        Task<CommunityProgram> AddCommunityProgram(CommunityProgram program);
+        Task<CommunityProgram> UpdateCommunityProgram(CommunityProgram program);
+        Task DeleteCommunityProgram(Guid communityProgramId);
+    }
+}
