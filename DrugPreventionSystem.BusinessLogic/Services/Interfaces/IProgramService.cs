f7b0e8e8e1cf2a7b6c1f231e17789166ad22085b
Nguyễn Minh Tuấn <vantam4578@gmail.com>
2025-07-01 23:51:11 +0700

    Add RegisterForProgramAsync,GetProgramsUserEnrolledAsync,SubmitProgramFeedbackAsync

---
+++ DrugPreventionSystem.BusinessLogic/Services/Interfaces/IProgramService.cs
@@ -0,0 +1,19 @@
+using DrugPreventionSystem.DataAccess.Models;
+using DrugPreventionSystem.BusinessLogic.Models.Request.ProgramParticipant;
+using DrugPreventionSystem.BusinessLogic.Models.Request.ProgramFeedback;
+using DrugPreventionSystem.BusinessLogic.Models.Responses;
+using DrugPreventionSystem.BusinessLogic.Models.Responses.CommunityProgram;
+using DrugPreventionSystem.BusinessLogic.Models.Responses.ProgramFeedback;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DrugPreventionSystem.BusinessLogic.Services.Interfaces
+{
+    public interface IProgramService
+    {
+        Task<ProgramParticipantResponse> RegisterForProgramAsync(ProgramParticipantCreateRequest request);
+        Task<IEnumerable<CommunityProgramResponse>> GetProgramsUserEnrolledAsync(Guid userId);
+        Task<ProgramFeedbackResponse> SubmitProgramFeedbackAsync(ProgramFeedbackCreateRequest request);
+    }
+} 
