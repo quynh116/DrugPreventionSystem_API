80e5ca953db0ecf1badef197bcd2185d1193f1b1
ThinhNguyen <thinhnguyen123258@gmail.com>
2025-07-02 10:26:05 +0700

    Add blogService and request, response

---
+++ DrugPreventionSystem.BusinessLogic/Models/Request/Blog/BlogUpdateRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugPreventionSystem.BusinessLogic.Models.Request.Blog
+{
+    public class BlogUpdateRequest
+    {
+        public string Title { get; set; }
+        public string? Content { get; set; }
+        public string? Excerpt { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public Guid? CategoryId { get; set; }
+        public string? Tags { get; set; }
+        public string Status { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
