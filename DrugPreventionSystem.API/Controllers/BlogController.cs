7030908fca1c739bd7792e9de85a7a6c7bb29eb8
ThinhNguyen <thinhnguyen123258@gmail.com>
2025-07-04 22:08:50 +0700

    Add BlogService and Controller

---
+++ DrugPreventionSystem.API/Controllers/BlogController.cs
@@ -0,0 +1,72 @@
+﻿using DrugPreventionSystem.BusinessLogic.Commons;
+using DrugPreventionSystem.BusinessLogic.Models.Request.Blog;
+using DrugPreventionSystem.BusinessLogic.Models.Responses;
+using DrugPreventionSystem.BusinessLogic.Models.Responses.Blog;
+using DrugPreventionSystem.BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrugPreventionSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : BaseApiController
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Result<BlogResponse>>> AddAsync([FromBody] BlogCreateRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToArray();
+                return BadRequest(Result<BlogResponse>.Invalid("Invalid blog data", errors));
+            }
+
+            var result = await _blogService.CreateAsync(request);
+            return HandleResult(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Result<IEnumerable<BlogResponse>>>> GetAllAsync()
+        {
+            var result = await _blogService.GetAllAsync();
+            return HandleResult(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Result<BlogResponse>>> GetByIdAsync(Guid id)
+        {
+            var result = await _blogService.GetByIdAsync(id);
+            return HandleResult(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Result<BlogResponse>>> UpdateAsync(Guid id, [FromBody] BlogUpdateRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToArray();
+                return BadRequest(Result<BlogResponse>.Invalid("Invalid update data", errors));
+            }
+
+            var result = await _blogService.UpdateAsync(request, id);
+            return HandleResult(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Result<bool>>> DeleteAsync(Guid id)
+        {
+            var result = await _blogService.DeleteAsync(id);
+            if (result.ResultStatus == ResultStatus.Success)
+            {
+                return NoContent();
+            }
+            return HandleResult(result);
+        }
+    }
+}
